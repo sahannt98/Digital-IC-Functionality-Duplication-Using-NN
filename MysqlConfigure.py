15635d2622b256702aef28b6e5044ee94eef0367
Ayesh-Rajakaruna <ayeshrajakaruna@gmail.com>
2022-11-18 17:22:27 +0530

    Make table name problem

--- MysqlConfigure.py
+++ MysqlConfigure.py
@@ -14,7 +14,7 @@ class MysqlConfigure:
         self.__Uplode_Data(X,Y,Table_name)
 
     def GetData(self,Table_name):
-        self.mycursor.execute(f"SELECT Input, Output FROM {Table_name}")
+        self.mycursor.execute(f"SELECT Input, Output FROM {Table_name.lower()}")
         myresult = self.mycursor.fetchall()
         X_ = []
         Y_ = []
@@ -38,16 +38,16 @@ class MysqlConfigure:
     def __createTabale(self,Table_name):
         self.mycursor.execute("SHOW TABLES")
         for x in self.mycursor:
-            if x[0] == f"{Table_name}":
-                self.mycursor.execute(f"DROP TABLE {Table_name}")
+            if x[0] == f"{Table_name.lower()}":
+                self.mycursor.execute(f"DROP TABLE {Table_name.lower()}")
                 break
-        self.mycursor.execute(f"CREATE TABLE {Table_name} (Number INT AUTO_INCREMENT PRIMARY KEY, Input VARCHAR(255), Output VARCHAR(255))")
+        self.mycursor.execute(f"CREATE TABLE {Table_name.lower()} (Number INT AUTO_INCREMENT PRIMARY KEY, Input VARCHAR(255), Output VARCHAR(255))")
 
     def __Uplode_Data(self,X_,Y_,Table_name):
         for i in range(len(X_)):
             x = X_[i]
             y = Y_[i]
-            self.mycursor.execute(f"INSERT INTO {Table_name}(Input, Output) VALUES ('{self.__ArraytoSting(x,self.input_len)}', '{self.__ArraytoSting(y,self.output_len)}')")
+            self.mycursor.execute(f"INSERT INTO {Table_name.lower()}(Input, Output) VALUES ('{self.__ArraytoSting(x,self.input_len)}', '{self.__ArraytoSting(y,self.output_len)}')")
             self.mydb.commit()
         
     def __ArraytoSting(self, ArrayofBinary,n):
